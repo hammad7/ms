51a99197f37897ede3a67990c4a923fbdc136e65
hammad7 <hammad73m@gmail.com>
2020-12-26 19:11:57 +0530

    added matrices info

--- ms.py
+++ ms.py
@@ -271,6 +271,12 @@ data.loc[(data.Year < 2015), ['Mileage']] = 22
 df.reset_index(inplace=True,drop=True) ### for continues index, eg after dropping
 
 
+
+#### compacting continuous "Size" series to bins
+inp1["Size_Bucket"] = pd.qcut(inp1.Size, [0,0.2,0.4,0.6,0.8,1.0],["VL","L","M","H","VH"])
+
+
+
 ### Plotting
 sns.set_style("dark")
 #OR
@@ -287,13 +293,24 @@ sns.distplot(inp1.Rating) ## pdf, gaussian density, ~ histogram
 plt.show()
 
 
+### 1d cntious , goupby categorical
+inp1.groupby(["Content Rating"])["Rating"].mean().plot.bar()
+inp1.groupby(["Content Rating"])["Rating"].median().plot.bar()
+## OR
+sns.barplot(data=inp1,x="Content Rating",y="Rating",estimator=np.mean)
+sns.barplot(data=inp1,x="Content Rating",y="Rating",estimator= lambda x: np.quantile(x,0.05)) ###### top 5th percentile
+
+plt.figure(figsize=[9,7])
+sns.boxplot(data=inp1,x="Content Rating",y="Rating")
+
+
 
 
 ###### categorical, 1d , asif value_counts() 
 sns.countplot(inp1["Android Ver"]) #1
 # https://colab.research.google.com/drive/1qh5jGes0fO2Cj3fJgH7fa2erMZyQE1-k#scrollTo=7xahaSI5xnjF
-inp1[‘Content Rating’].value_counts().plot.bar() #2
-inp1["Content Rating"].value_counts().plot.pie() #3
+inp1[‘Content Rating’].value_counts().plot.bar() #2  better then pie
+inp1["Content Rating"].value_counts().plot.pie() #3   
 
 import scipy.stats as stats
 # continuos, 2d -scatter plot
@@ -303,3 +320,93 @@ sns.jointplot(x=inp1.Size,y=inp1.Rating,kind="reg") ## with regressio line
 
 ## pair plot, both kind vars
 sns.pairplot(inp1)
+
+
+## 3 variables at a time , Eg: 1 continous, 2 categorical
+# uses pivot table
+pd.pivot_table(inp1, index = "Content Rating", columns = "Size_Bucket", values = "Rating")
+pd.pivot_table(inp1, index = "Content Rating", columns = "Size_Bucket", values = "Rating",aggfunc=np.median)
+res = pd.pivot_table(inp1, index = "Content Rating", columns = "Size_Bucket", values = "Rating",aggfunc=lambda x : np.quantile(x,0.2))
+sns.heatmap(res,cmap="Greens",annot=True)
+
+## stackedBar chart
+monthly = pd.pivot_table(inp1,index= ["updated_month"],columns=["Content Rating"],values="Installs", aggfunc = sum)
+monthly
+monthly.plot(kind = "bar", stacked=True,figsize=[10,6])
+## stacked proportions
+monthly_perc = monthly[["Everyone","Everyone 10+","Mature 17+","Teen"]].apply(lambda x : x/x.sum(),axis=1)
+monthly_perc
+monthly_perc.plot(kind = "bar", stacked=True,figsize=[10,6])
+
+
+## line plot, 2 continuos
+inp1.groupby(["intCol"])["floatCol"].mean().plot()
+
+
+
+res = inp1.groupby(["updated_month"])[['Rating']].mean()
+res.reset_index(inplace=True)
+## interactive, plotly - https://plot.ly/python/getting-started/
+import plotly.express as px
+fig = px.line(res, x="updated_month",y="Rating",title="Montly average rating")
+fig.show()
+
+
+
+### Solve system of linear euqtion using inverse
+# system of three equations
+A = np.array([[1, 5, -1], 
+              [2, 3, -2], 
+              [-3, 4, 0]])
+b = np.array([1, 2, 0])
+
+# compute the inverse
+A_inv = np.linalg.inv(A)
+
+# solution: A_inv * b
+x = np.dot(A_inv, b)  #### inverse transform of A on b, that is what x is #https://www.youtube.com/watch?v=uQhTuRlWMxw
+print(x) # returns [ 0.  0. -1.]
+
+## mtraices represent space tranformations
+
+### in inverse squishes the b to smaller dimension (line), its det usually 0 and no solution exists
+# https://math.stackexchange.com/questions/21100/importance-of-matrix-rank
+# Eg: rank 1 mtric [[1,2],[3,6]], will map all points of form (x,y) into line (c,3c)
+# transformation that reduces inforamtion
+
+#Also
+# rank of a matrix is the dimensionality of the output of the transformation, ## how mnay independent cols
+# rank is less then size when the two column vectors are collinear OR 1 column is a linear combinatio of others
+
+### column space of a matrix is the span of the columns of the matrix. 
+
+##null space is the space of all vectors that land on the zero vector under the transformation, EG: x=0, for Ax=0, end others
+
+# https://www.youtube.com/watch?v=v8VSDg_WQlA
+#### non square matrice intution,2X3 matric maps 2d space to 3d space
+
+# least quare approx = https://www.youtube.com/watch?v=MC7l96tW8V8&list=PL39469144F25ACECE&index=3
+# x=(ATA)−1ATb. 
+
+###
+# eigenvectors of a matrix A are special vectors which do not change their direction (or span) under the transformation A. 
+#They just get scaled in the same direction, 
+#and the magnitude by which they get scaled is called the eigenvalue of that eigenvector.
+# Ax=λx where x is the eigenvector of the matrix A and λ is the eigenvalue
+# eigenvectors: det(A−λI)=0
+# Using the eigenvalues , solve Av=λv to find the eigenvectors v
+# calculate eigenvalues and eigenvectors
+A = np.array([[-1, 0], 
+              [0, 1]])
+
+# c is the array of eigenvalues
+# v is the matrix with eigenvectors as the columns
+c, v = np.linalg.eig(A)
+print(c)
+print(v)
+
+
+# Eigendecomposition of a Matrix
+# A=QΣQ−1
+ # where Q is a matrix whose each column is an eigenvector of A  (hence independent)
+ # and Σ is a diagonal matrix whose diagonal entries are the eigenvalues of A
